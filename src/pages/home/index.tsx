73977ef89e3f3527c1aeef15a73bb6b2ed82445b
Rafael Valverde <rafaverde@msn.com>
2025-02-24 18:50:23 -0300

    Home page initial layout elements created.

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+import previewImg from "../../assets/app-preview.png";
+import { Container, Hero, Preview } from "./styles";
+import { Heading, Text } from "@ignite-ui/react";
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size={"4xl"}>Agendamento descomplicado</Heading>
+        <Text size={"xl"}>
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImg}
+          alt="Calendário simbolizando a aplicação em funcionamento."
+          height={400}
+          quality={100}
+          priority
+        ></Image>
+      </Preview>
+    </Container>
+  );
+}

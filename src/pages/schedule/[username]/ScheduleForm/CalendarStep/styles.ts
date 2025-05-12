cb178f66cfd7ea24f32b3f2fab969cf5ed838bdf
Rafael Valverde <rafaverde@msn.com>
2025-05-12 13:48:09 -0300

    Calendar layout created.

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -4,6 +4,10 @@ export const Container = styled(Box, {
   margin: "$6 auto 0",
   padding: 0,
   display: "grid",
+
   maxWidth: "100%",
   position: "relative",
+
+  width: 540,
+  gridTemplateColumns: "1fr",
 });

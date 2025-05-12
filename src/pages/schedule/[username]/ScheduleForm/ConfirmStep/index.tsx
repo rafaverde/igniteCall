0ea77b1a4b29762084baa4e4863b1b29d8ab336f
Rafael Valverde <rafaverde@msn.com>
2025-05-12 17:20:25 -0300

    Implement ConfirmStep component with form fields and styling for scheduling confirmation

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,44 @@
+import { Button, Text, TextArea, TextInput } from "@ignite-ui/react";
+import { ConfirmForm, FormActions, FormHeader } from "./styles";
+import { CalendarBlank, Clock } from "@phosphor-icons/react";
+
 export function ConfirmStep() {
-  return;
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          12 de maio de 2025
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome Completo</Text>
+        <TextInput type="text" placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="Seu e-mail" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  );
 }
